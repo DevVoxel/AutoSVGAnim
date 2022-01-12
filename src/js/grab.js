c8fcd9ca41e084a18353e03d9c22067111308d50
voxel20 <aidensmith20@gmail.com>
2022-01-11 19:14:58 -0500

    Removed Unique SVG id to make it work universally.

--- src/js/grab.js
+++ src/js/grab.js
@@ -1,12 +1,12 @@
-const vox = document.querySelectorAll('#vox path');
+const svg = document.querySelectorAll('svg path');
 
-for (let i = 0; i < vox.length; i++) {
-    let dim = `${ vox[i].getTotalLength() }`;
+for (let i = 0; i < svg.length; i++) {
+    let dim = `${ svg[i].getTotalLength() }`;
     console.log(dim);
     let edit = document.styleSheets[0];
     edit.insertRule(":root {--strokeDimension" + `${i}` + ":" + dim + ";}");
 
     let insert = document.styleSheets[0];
-    insert.insertRule("#vox path:nth-child(" + `${i}` + ") {stroke-dasharray: --strokeDimension" + `${i}` + ";stroke-dashoffset: --strokeDimension" + `${i}` + "; animation: line-anim 7s ease forwards;}");
+    insert.insertRule("svg path:nth-child(" + `${i}` + ") {stroke-dasharray: --strokeDimension" + `${i}` + ";stroke-dashoffset: --strokeDimension" + `${i}` + "; animation: line-anim 7s ease forwards;}");
 }
 
